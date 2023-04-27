88702efc8eef592a549f1b9db99425f5dbf74380
Daniel Barros <daniel.barros@cqumail.com>
2023-04-28 00:11:32 +0800

    feature: Implement Customer Search.

--- CMSNetBeans/src/main/java/CMS/UserInterfaceController.java
+++ CMSNetBeans/src/main/java/CMS/UserInterfaceController.java
@@ -1,8 +1,15 @@
 package CMS;
 
+import CMS.Util.DatabaseHandler;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -10,11 +17,64 @@ import javafx.fxml.Initializable;
  * @author Daniel Barros
  */
 public class UserInterfaceController implements Initializable {
+    
+    @FXML
+    private TextField tfCustomerID;
+    @FXML
+    private TextField tfFirstName;
+    @FXML
+    private TextField tfLastName;
+    @FXML
+    private TextField tfContactNumber;
+    @FXML
+    private TextField tfEmail;
+    @FXML
+    private TextArea taAddress;
+//    @FXML
+//    private TextField product?;
+//    @FXML
+//    private TextField customerType?;
+    
+    private String customerID;
+    private String customerFirstName;
+    private String customerLastName;
+    private String customerContactNumber;
+    private String customerEmail;
+    private String customerAddress;
+    
+    @FXML
+    public void customerSearchButtonClick() {
+        System.out.println("Search Button pressed.");
+        tfFirstName.clear();
+        tfLastName.clear();
+        tfContactNumber.clear();
+        tfEmail.clear();
+        taAddress.clear();
+        try (Connection connection = DatabaseHandler.getConnection()) {
+            System.out.println("Connection successful.");
+            customerID = tfCustomerID.getText();
+            PreparedStatement query = connection.prepareStatement(
+                "SELECT * FROM customers WHERE custID = " + customerID + ";"
+            );
+            System.out.println("Query generation successful.");
+            ResultSet results = query.executeQuery();
+            System.out.println("Query execution successful.");
+            if (results.next()) {
+                tfFirstName.setText(results.getString("fName"));
+                tfLastName.setText(results.getString("lName"));
+                tfContactNumber.setText(results.getString("mobile"));
+                tfEmail.setText(results.getString("email"));
+                taAddress.setText(results.getString("addr"));
+            }
+        } catch (Exception e) {
+            System.out.println("Connection failed.");
+        }
+    }
 
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+        // Empty method.
     }    
     
 }
